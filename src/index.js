20f0886bd06dfc2b428475ae496e2f7d8a71dfbf
NECA <nickesteban100mail.com>
2026-04-16 17:43:42 -0500

    Se realiza la primera peticion GET al local host por medio de postman

--- src/index.js
+++ src/index.js
@@ -3,7 +3,17 @@ import express from "express";
 const app=express();
 const PORT = 3000;
 
-console.log("inicializando servidos...")
+console.log("inicializando servidor...")
+
+app.get("/users",(req,res)=>{
+    try{
+        console.log("usuarios enviados otra vez perra")
+        res.send("Envio de Usuarios")
+    }catch(error){
+        console.log(`tene un error: ${error}`);
+        res.send("No se pusieron obtener los usuarios");
+    }
+})
 
 app.listen( PORT, ()=>{
     console.log(`Servidor escuchando en el puerto ${PORT}`);
